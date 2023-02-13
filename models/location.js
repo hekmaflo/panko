5aac9ae501f2812a94030186b23c9ed7298d332d
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-02-13 10:52:13 -0600

    added first schema

--- models/location.js
+++ models/location.js
@@ -2,9 +2,9 @@ const { Schema } = require("mongoose");
 
 const locationSchema = new Schema(
   {
-    issueType: { type: String, required: true },
-    subject: { type: String, required: true },
-    message: { type: String, required: true },
+    locationName: { type: String, required: true },
+    address: { type: String, required: true },
+    description: { type: String, required: true },
   },
   { timestamps: true }
 );
